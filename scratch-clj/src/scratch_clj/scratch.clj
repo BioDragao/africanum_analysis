8b72205968f17f1f7d6140c5aa0216c2b75db30c
Abhinav Sharma <abhi18av@outlook.com>
2019-05-28 10:58:28 +0530

    working on the actual analysis via python files

--- scratch-clj/src/scratch_clj/scratch.clj
+++ scratch-clj/src/scratch_clj/scratch.clj
@@ -1,2 +1,102 @@
-(ns scratch-clj.scratch)
+(ns scratch-clj.scratch
+  (:require [clojure.string :as str]
+            [clojure.set :as set]))
 
+(defn vec-remove
+  "remove elem in coll"
+  [coll pos]
+  (vec (concat (subvec coll 0 pos) (subvec coll (inc pos)))))
+
+(def whitelisted-genomes
+  (apply vector
+         (apply hash-set (map
+                          (fn [x]
+                            (first
+                             (str/split x #"\t")))
+                          (str/split-lines
+                           (slurp "../genomes-abhi-list.txt"))))))
+
+(def redlisted-genomes
+  (apply vector
+         (apply hash-set (map
+                          (fn [x]
+                            (first
+                             (str/split x #"\t")))
+                          (str/split-lines
+                           (slurp "../genomes-ena-list.txt"))))))
+
+(def yellowlisted-genomes
+  (vec-remove
+   (apply vector
+          (apply hash-set (map
+                           (fn [x]
+                             (first
+                              (str/split x #"\t")))
+                           (str/split-lines
+                            (slurp "../genomes-em-list.txt"))))) 17))
+
+
+(comment
+  (first whitelisted-genomes)
+  (first redlisted-genomes)
+  (first yellowlisted-genomes)
+)
+
+
+(def genomes-in-abhi-folder
+  (apply vector
+         (apply hash-set (map
+                          (fn [x]
+                            (first (str/split x #"_")))
+                          (str/split-lines
+                           (slurp "../genomes_abhi/_list_of_files.txt"))))))
+
+(def genomes-in-em-folder
+  (apply vector
+         (apply hash-set (map
+                          (fn [x]
+                            (first (str/split x #"_")))
+                          (str/split-lines
+                           (slurp "../genomes_em/_list_of_files.txt"))))))
+
+(def genomes-in-ena-folder
+  (apply vector
+         (apply hash-set (map
+                          (fn [x]
+                            (first (str/split x #"_")))
+                          (str/split-lines
+                           (slurp "../genomes_ena/_list_of_files.txt"))))))
+
+(comment
+  (first genomes-in-abhi-folder)
+  (first genomes-in-em-folder)
+  (first genomes-in-ena-folder)
+)
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(def all-downloaded-genomes )
+
+
+(clojure.set/intersection
+(apply hash-set genomes-in-abhi-folder)
+(apply hash-set genomes-in-em-folder))
+
+
+(clojure.set/intersection
+ (apply hash-set genomes-in-abhi-folder)
+ (apply hash-set genomes-in-ena-folder))
+
+
+(clojure.set/intersection
+ (apply hash-set genomes-in-em-folder)
+ (apply hash-set genomes-in-ena-folder))
+
+
+
+
+
+(def all-excel-listed-genomes )
