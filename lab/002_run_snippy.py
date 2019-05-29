46ed089d04eb62bb7670d71ebb7cc616f2320b79
Abhinav Sharma <abhi18av@outlook.com>
2019-05-29 11:17:16 +0530

    finalized the 002 python file as well

--- lab/002_run_snippy.py
+++ lab/002_run_snippy.py
@@ -11,7 +11,10 @@ def run_snippy(a_pair):
 
     # snippy --cpus 4 --outdir Mcanettii --ref ./NC000962_3.gbk --R1 ./Mcanettii_R1.fastq.gz --R2 ./Mcanettii_R2.fastq.gz
 
-    genome_name = a_pair[0].split("_")[0]
+    #genome_name = a_pair[0].split("_")[0]
+    genome_name = "_".join(a_pair[0].split("_")[:-1])
+
+    # print(genome_name)
 
     genome_1 = a_pair[0]
 
