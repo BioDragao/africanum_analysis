7b1c665a6e3f9c6d65ebaa0b808fffbe3818f50b
Abhinav Sharma <abhi18av@outlook.com>
2019-05-30 18:05:20 +0000

    added the complete analyze-upload-delete command

---
+++ lab/upload_to_em_snippy_folder.sh
@@ -0,0 +1,177 @@
+#rclone copy ./SAMEA2627044 onedrive-em:africanum_analysis/snippy/SAMEA2627044/ -vv
+
+rclone copy ./SAMEA1877109 onedrive-em:africanum_analysis/snippy/SAMEA1877109 -vv
+
+rm -rf SAMEA1877109_1.fastq.gz
+rm -rf SAMEA1877109_2.fastq.gz
+
+rm -rf SAMEA1877109 
+
+rclone copy ./SAMEA1877112_L1 onedrive-em:africanum_analysis/snippy/SAMEA1877112_L1 -vv
+
+rm -rf SAMEA1877112_L1_1.fastq.gz
+rm -rf SAMEA1877112_L1_2.fastq.gz
+
+rm -rf SAMEA1877112_L1 
+
+rclone copy ./SAMEA1877117 onedrive-em:africanum_analysis/snippy/SAMEA1877117 -vv
+
+rm -rf SAMEA1877117_1.fastq.gz
+rm -rf SAMEA1877117_2.fastq.gz
+
+rm -rf SAMEA1877117 
+
+rclone copy ./SAMEA1877126 onedrive-em:africanum_analysis/snippy/SAMEA1877126 -vv
+
+rm -rf SAMEA1877126_1.fastq.gz
+rm -rf SAMEA1877126_2.fastq.gz
+
+rm -rf SAMEA1877126 
+
+rclone copy ./SAMEA1877132 onedrive-em:africanum_analysis/snippy/SAMEA1877132 -vv
+
+rm -rf SAMEA1877132_1.fastq.gz
+rm -rf SAMEA1877132_2.fastq.gz
+
+rm -rf SAMEA1877132 
+
+rclone copy ./SAMEA1877136 onedrive-em:africanum_analysis/snippy/SAMEA1877136 -vv
+
+rm -rf SAMEA1877136_1.fastq.gz
+rm -rf SAMEA1877136_2.fastq.gz
+
+rm -rf SAMEA1877136 
+
+rclone copy ./SAMEA1877139 onedrive-em:africanum_analysis/snippy/SAMEA1877139 -vv
+
+rm -rf SAMEA1877139_1.fastq.gz
+rm -rf SAMEA1877139_2.fastq.gz
+
+rm -rf SAMEA1877139 
+
+rclone copy ./SAMEA1877142 onedrive-em:africanum_analysis/snippy/SAMEA1877142 -vv
+
+rm -rf SAMEA1877142_1.fastq.gz
+rm -rf SAMEA1877142_2.fastq.gz
+
+rm -rf SAMEA1877142 
+
+rclone copy ./SAMEA1877150 onedrive-em:africanum_analysis/snippy/SAMEA1877150 -vv
+
+rm -rf SAMEA1877150_1.fastq.gz
+rm -rf SAMEA1877150_2.fastq.gz
+
+rm -rf SAMEA1877150 
+
+rclone copy ./SAMEA1877150_L6 onedrive-em:africanum_analysis/snippy/SAMEA1877150_L6 -vv
+
+rm -rf SAMEA1877150_L6_1.fastq.gz
+rm -rf SAMEA1877150_L6_2.fastq.gz
+
+rm -rf SAMEA1877150_L6 
+
+rclone copy ./SAMEA1877152 onedrive-em:africanum_analysis/snippy/SAMEA1877152 -vv
+
+rm -rf SAMEA1877152_1.fastq.gz
+rm -rf SAMEA1877152_2.fastq.gz
+
+rm -rf SAMEA1877152 
+
+rclone copy ./SAMEA1877165 onedrive-em:africanum_analysis/snippy/SAMEA1877165 -vv
+
+rm -rf SAMEA1877165_1.fastq.gz
+rm -rf SAMEA1877165_2.fastq.gz
+
+rm -rf SAMEA1877165 
+
+rclone copy ./SAMEA1877169 onedrive-em:africanum_analysis/snippy/SAMEA1877169 -vv
+
+rm -rf SAMEA1877169_1.fastq.gz
+rm -rf SAMEA1877169_2.fastq.gz
+
+rm -rf SAMEA1877169 
+
+rclone copy ./SAMEA1877169_L5 onedrive-em:africanum_analysis/snippy/SAMEA1877169_L5 -vv
+
+rm -rf SAMEA1877169_L5_1.fastq.gz
+rm -rf SAMEA1877169_L5_2.fastq.gz
+
+rm -rf SAMEA1877169_L5 
+
+rclone copy ./SAMEA1877177 onedrive-em:africanum_analysis/snippy/SAMEA1877177 -vv
+
+rm -rf SAMEA1877177_1.fastq.gz
+rm -rf SAMEA1877177_2.fastq.gz
+
+rm -rf SAMEA1877177 
+
+rclone copy ./SAMEA1877181_L3 onedrive-em:africanum_analysis/snippy/SAMEA1877181_L3 -vv
+
+rm -rf SAMEA1877181_L3_1.fastq.gz
+rm -rf SAMEA1877181_L3_2.fastq.gz
+
+rm -rf SAMEA1877181_L3 
+
+rclone copy ./SAMEA1877187 onedrive-em:africanum_analysis/snippy/SAMEA1877187 -vv
+
+rm -rf SAMEA1877187_1.fastq.gz
+rm -rf SAMEA1877187_2.fastq.gz
+
+rm -rf SAMEA1877187 
+
+rclone copy ./SAMEA1877190 onedrive-em:africanum_analysis/snippy/SAMEA1877190 -vv
+
+rm -rf SAMEA1877190_1.fastq.gz
+rm -rf SAMEA1877190_2.fastq.gz
+
+rm -rf SAMEA1877190 
+
+rclone copy ./SAMEA1877206 onedrive-em:africanum_analysis/snippy/SAMEA1877206 -vv
+
+rm -rf SAMEA1877206_1.fastq.gz
+rm -rf SAMEA1877206_2.fastq.gz
+
+rm -rf SAMEA1877206 
+
+rclone copy ./SAMEA1877207 onedrive-em:africanum_analysis/snippy/SAMEA1877207 -vv
+
+rm -rf SAMEA1877207_1.fastq.gz
+rm -rf SAMEA1877207_2.fastq.gz
+
+rm -rf SAMEA1877207 
+
+rclone copy ./SAMEA1877219_L2 onedrive-em:africanum_analysis/snippy/SAMEA1877219_L2 -vv
+
+rm -rf SAMEA1877219_L2_1.fastq.gz
+rm -rf SAMEA1877219_L2_2.fastq.gz
+
+rm -rf SAMEA1877219_L2 
+
+rclone copy ./SAMEA1877227 onedrive-em:africanum_analysis/snippy/SAMEA1877227 -vv
+
+rm -rf SAMEA1877227_1.fastq.gz
+rm -rf SAMEA1877227_2.fastq.gz
+
+rm -rf SAMEA1877227 
+
+rclone copy ./SAMEA1877229 onedrive-em:africanum_analysis/snippy/SAMEA1877229 -vv
+
+rm -rf SAMEA1877229_1.fastq.gz
+rm -rf SAMEA1877229_2.fastq.gz
+
+rm -rf SAMEA1877229 
+
+rclone copy ./SAMEA1877233 onedrive-em:africanum_analysis/snippy/SAMEA1877233 -vv
+
+rm -rf SAMEA1877233_1.fastq.gz
+rm -rf SAMEA1877233_2.fastq.gz
+
+rm -rf SAMEA1877233 
+
+rclone copy ./SAMEA1877237 onedrive-em:africanum_analysis/snippy/SAMEA1877237 -vv
+
+rm -rf SAMEA1877237_1.fastq.gz
+rm -rf SAMEA1877237_2.fastq.gz
+
+rm -rf SAMEA1877237 
+
