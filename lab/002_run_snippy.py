b14b0f837b650296eb6d659e4611ec63894c2af3
Abhinav Sharma <abhi18av@outlook.com>
2019-05-30 10:26:41 +0530

    added list partition code to 002_snippy

--- lab/002_run_snippy.py
+++ lab/002_run_snippy.py
@@ -1,10 +1,36 @@
 import json
 import os
 import shutil
+from itertools import islice
 
-# all_fastq_files = list(filter(lambda x:has_fastq_in_name(x), all_files))
-with open("./_all_lab_genome_files.json", 'r') as f:
-    genome_pairs = json.loads(f.read())
+# with open("./_all_lab_genome_files.json", 'r') as f:
+#     genome_pairs = json.loads(f.read())
+
+# TODO create pairs of genomes based on same first name
+
+all_files = [f for f in os.listdir() if os.path.isfile(f)]
+
+
+def has_fastq_in_name(string):
+    if (string.find("fastq") == -1):
+        #print("NO")
+        return 0
+    else:
+        #print("YES")
+        return 1
+
+
+all_fastq_files = list(filter(lambda x: has_fastq_in_name(x), all_files))
+
+all_fastq_files.sort()
+
+
+def chunk(it, size):
+    it = iter(it)
+    return iter(lambda: tuple(islice(it, size)), ())
+
+
+genome_pairs = list(chunk(all_fastq_files, 2))
 
 
 def run_snippy(a_pair):
