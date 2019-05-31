46594e0840037969fafded16d463cc9636e9e4ec
abhi18av <abhi18av@gmail.com>
2019-05-31 07:59:06 +0000

    updated genomes sh file

--- lab/002_genomes.sh
+++ lab/002_genomes.sh
@@ -392,155 +392,155 @@
 #DONE
 #############
 
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA1877109_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA1877109_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA1877112_L1_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA1877112_L1_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA1877117_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA1877117_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA1877126_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA1877126_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA1877132_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA1877132_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA1877136_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA1877136_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA1877139_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA1877139_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA1877142_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA1877142_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA1877150_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA1877150_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA1877150_L6_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA1877150_L6_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA1877152_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA1877152_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA1877165_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA1877165_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA1877169_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA1877169_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA1877169_L5_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA1877169_L5_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA1877177_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA1877177_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA1877181_L3_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA1877181_L3_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA1877187_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA1877187_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA1877190_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA1877190_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA1877206_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA1877206_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA1877207_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA1877207_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA1877219_L2_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA1877219_L2_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA1877227_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA1877227_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA1877229_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA1877229_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA1877233_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA1877233_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA1877237_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA1877237_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA1877266_L4_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA1877266_L4_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA1877285_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA1877285_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340874_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340874_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340875_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340875_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340876_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340876_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340880_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340880_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340881_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340881_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340882_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340882_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340888_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340888_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340889_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340889_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340890_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340890_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340891_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340891_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340892_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340892_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340893_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340893_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340894_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340894_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340896_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340896_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340897_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340897_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340900_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340900_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340901_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340901_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340903_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340903_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340904_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340904_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340905_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340905_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340907_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340907_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340908_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340908_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340909_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340909_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340910_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340910_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340912_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340912_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340913_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340913_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340914_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340914_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340915_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340915_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340916_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340916_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340917_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340917_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340918_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340918_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340919_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340919_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340920_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340920_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340921_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340921_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340922_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340922_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340923_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340923_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340924_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340924_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340929_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340929_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340930_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340930_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340931_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340931_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340932_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340932_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340933_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340933_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340935_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340935_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340936_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340936_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340938_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340938_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340939_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340939_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340940_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340940_2.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340941_1.fastq.gz . -vv
-#rclone copy onedrive-em:africanum_analysis/lab/SAMEA2340941_2.fastq.gz . -vv
+rm -rf SAMEA1877109_1.fastq.gz 
+rm -rf SAMEA1877109_2.fastq.gz 
+rm -rf SAMEA1877112_L1_1.fastq.gz 
+rm -rf SAMEA1877112_L1_2.fastq.gz 
+rm -rf SAMEA1877117_1.fastq.gz 
+rm -rf SAMEA1877117_2.fastq.gz 
+rm -rf SAMEA1877126_1.fastq.gz 
+rm -rf SAMEA1877126_2.fastq.gz 
+rm -rf SAMEA1877132_1.fastq.gz 
+rm -rf SAMEA1877132_2.fastq.gz 
+rm -rf SAMEA1877136_1.fastq.gz 
+rm -rf SAMEA1877136_2.fastq.gz 
+rm -rf SAMEA1877139_1.fastq.gz 
+rm -rf SAMEA1877139_2.fastq.gz 
+rm -rf SAMEA1877142_1.fastq.gz 
+rm -rf SAMEA1877142_2.fastq.gz 
+rm -rf SAMEA1877150_1.fastq.gz 
+rm -rf SAMEA1877150_2.fastq.gz 
+rm -rf SAMEA1877150_L6_1.fastq.gz 
+rm -rf SAMEA1877150_L6_2.fastq.gz 
+rm -rf SAMEA1877152_1.fastq.gz 
+rm -rf SAMEA1877152_2.fastq.gz 
+rm -rf SAMEA1877165_1.fastq.gz 
+rm -rf SAMEA1877165_2.fastq.gz 
+rm -rf SAMEA1877169_1.fastq.gz 
+rm -rf SAMEA1877169_2.fastq.gz 
+rm -rf SAMEA1877169_L5_1.fastq.gz 
+rm -rf SAMEA1877169_L5_2.fastq.gz 
+rm -rf SAMEA1877177_1.fastq.gz 
+rm -rf SAMEA1877177_2.fastq.gz 
+rm -rf SAMEA1877181_L3_1.fastq.gz 
+rm -rf SAMEA1877181_L3_2.fastq.gz 
+rm -rf SAMEA1877187_1.fastq.gz 
+rm -rf SAMEA1877187_2.fastq.gz 
+rm -rf SAMEA1877190_1.fastq.gz 
+rm -rf SAMEA1877190_2.fastq.gz 
+rm -rf SAMEA1877206_1.fastq.gz 
+rm -rf SAMEA1877206_2.fastq.gz 
+rm -rf SAMEA1877207_1.fastq.gz 
+rm -rf SAMEA1877207_2.fastq.gz 
+rm -rf SAMEA1877219_L2_1.fastq.gz 
+rm -rf SAMEA1877219_L2_2.fastq.gz 
+rm -rf SAMEA1877227_1.fastq.gz 
+rm -rf SAMEA1877227_2.fastq.gz 
+rm -rf SAMEA1877229_1.fastq.gz 
+rm -rf SAMEA1877229_2.fastq.gz 
+rm -rf SAMEA1877233_1.fastq.gz 
+rm -rf SAMEA1877233_2.fastq.gz 
+rm -rf SAMEA1877237_1.fastq.gz 
+rm -rf SAMEA1877237_2.fastq.gz 
+rm -rf SAMEA1877266_L4_1.fastq.gz 
+rm -rf SAMEA1877266_L4_2.fastq.gz 
+rm -rf SAMEA1877285_1.fastq.gz 
+rm -rf SAMEA1877285_2.fastq.gz 
+rm -rf SAMEA2340874_1.fastq.gz 
+rm -rf SAMEA2340874_2.fastq.gz 
+rm -rf SAMEA2340875_1.fastq.gz 
+rm -rf SAMEA2340875_2.fastq.gz 
+rm -rf SAMEA2340876_1.fastq.gz 
+rm -rf SAMEA2340876_2.fastq.gz 
+rm -rf SAMEA2340880_1.fastq.gz 
+rm -rf SAMEA2340880_2.fastq.gz 
+rm -rf SAMEA2340881_1.fastq.gz 
+rm -rf SAMEA2340881_2.fastq.gz 
+rm -rf SAMEA2340882_1.fastq.gz 
+rm -rf SAMEA2340882_2.fastq.gz 
+rm -rf SAMEA2340888_1.fastq.gz 
+rm -rf SAMEA2340888_2.fastq.gz 
+rm -rf SAMEA2340889_1.fastq.gz 
+rm -rf SAMEA2340889_2.fastq.gz 
+rm -rf SAMEA2340890_1.fastq.gz 
+rm -rf SAMEA2340890_2.fastq.gz 
+rm -rf SAMEA2340891_1.fastq.gz 
+rm -rf SAMEA2340891_2.fastq.gz 
+rm -rf SAMEA2340892_1.fastq.gz 
+rm -rf SAMEA2340892_2.fastq.gz 
+rm -rf SAMEA2340893_1.fastq.gz 
+rm -rf SAMEA2340893_2.fastq.gz 
+rm -rf SAMEA2340894_1.fastq.gz 
+rm -rf SAMEA2340894_2.fastq.gz 
+rm -rf SAMEA2340896_1.fastq.gz 
+rm -rf SAMEA2340896_2.fastq.gz 
+rm -rf SAMEA2340897_1.fastq.gz 
+rm -rf SAMEA2340897_2.fastq.gz 
+rm -rf SAMEA2340900_1.fastq.gz 
+rm -rf SAMEA2340900_2.fastq.gz 
+rm -rf SAMEA2340901_1.fastq.gz 
+rm -rf SAMEA2340901_2.fastq.gz 
+rm -rf SAMEA2340903_1.fastq.gz 
+rm -rf SAMEA2340903_2.fastq.gz 
+rm -rf SAMEA2340904_1.fastq.gz 
+rm -rf SAMEA2340904_2.fastq.gz 
+rm -rf SAMEA2340905_1.fastq.gz 
+rm -rf SAMEA2340905_2.fastq.gz 
+rm -rf SAMEA2340907_1.fastq.gz 
+rm -rf SAMEA2340907_2.fastq.gz 
+rm -rf SAMEA2340908_1.fastq.gz 
+rm -rf SAMEA2340908_2.fastq.gz 
+rm -rf SAMEA2340909_1.fastq.gz 
+rm -rf SAMEA2340909_2.fastq.gz 
+rm -rf SAMEA2340910_1.fastq.gz 
+rm -rf SAMEA2340910_2.fastq.gz 
+rm -rf SAMEA2340912_1.fastq.gz 
+rm -rf SAMEA2340912_2.fastq.gz 
+rm -rf SAMEA2340913_1.fastq.gz 
+rm -rf SAMEA2340913_2.fastq.gz 
+rm -rf SAMEA2340914_1.fastq.gz 
+rm -rf SAMEA2340914_2.fastq.gz 
+rm -rf SAMEA2340915_1.fastq.gz 
+rm -rf SAMEA2340915_2.fastq.gz 
+rm -rf SAMEA2340916_1.fastq.gz 
+rm -rf SAMEA2340916_2.fastq.gz 
+rm -rf SAMEA2340917_1.fastq.gz 
+rm -rf SAMEA2340917_2.fastq.gz 
+rm -rf SAMEA2340918_1.fastq.gz 
+rm -rf SAMEA2340918_2.fastq.gz 
+rm -rf SAMEA2340919_1.fastq.gz 
+rm -rf SAMEA2340919_2.fastq.gz 
+rm -rf SAMEA2340920_1.fastq.gz 
+rm -rf SAMEA2340920_2.fastq.gz 
+rm -rf SAMEA2340921_1.fastq.gz 
+rm -rf SAMEA2340921_2.fastq.gz 
+rm -rf SAMEA2340922_1.fastq.gz 
+rm -rf SAMEA2340922_2.fastq.gz 
+rm -rf SAMEA2340923_1.fastq.gz 
+rm -rf SAMEA2340923_2.fastq.gz 
+rm -rf SAMEA2340924_1.fastq.gz 
+rm -rf SAMEA2340924_2.fastq.gz 
+rm -rf SAMEA2340929_1.fastq.gz 
+rm -rf SAMEA2340929_2.fastq.gz 
+rm -rf SAMEA2340930_1.fastq.gz 
+rm -rf SAMEA2340930_2.fastq.gz 
+rm -rf SAMEA2340931_1.fastq.gz 
+rm -rf SAMEA2340931_2.fastq.gz 
+rm -rf SAMEA2340932_1.fastq.gz 
+rm -rf SAMEA2340932_2.fastq.gz 
+rm -rf SAMEA2340933_1.fastq.gz 
+rm -rf SAMEA2340933_2.fastq.gz 
+rm -rf SAMEA2340935_1.fastq.gz 
+rm -rf SAMEA2340935_2.fastq.gz 
+rm -rf SAMEA2340936_1.fastq.gz 
+rm -rf SAMEA2340936_2.fastq.gz 
+rm -rf SAMEA2340938_1.fastq.gz 
+rm -rf SAMEA2340938_2.fastq.gz 
+rm -rf SAMEA2340939_1.fastq.gz 
+rm -rf SAMEA2340939_2.fastq.gz 
+rm -rf SAMEA2340940_1.fastq.gz 
+rm -rf SAMEA2340940_2.fastq.gz 
+rm -rf SAMEA2340941_1.fastq.gz 
+rm -rf SAMEA2340941_2.fastq.gz 
 
 
