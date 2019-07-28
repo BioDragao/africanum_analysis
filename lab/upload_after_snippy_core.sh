75417514a9e9c092c3317e52e3030ca1026aa51b
abhi18av <abhi18av@gmail.com>
2019-07-28 21:10:39 +0000

    uploaded the snippy core files for non-yellow genomes

---
+++ lab/upload_after_snippy_core.sh
@@ -0,0 +1,6 @@
+rclone copy ./core.full.aln onedrive-em:africanum_analysis/snippy_core_output_28072019/  -v
+rclone copy ./core.aln onedrive-em:africanum_analysis/snippy_core_output_28072019/  -v
+rclone copy ./core.nway.tab onedrive-em:africanum_analysis/snippy_core_output_28072019/ -v
+rclone copy ./core.tab onedrive-em:africanum_analysis/snippy_core_output_28072019/ -v
+rclone copy ./core.txt onedrive-em:africanum_analysis/snippy_core_output_28072019/ -v
+rclone copy ./core.vcf onedrive-em:africanum_analysis/snippy_core_output_28072019/ -v
