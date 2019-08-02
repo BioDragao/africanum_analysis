ef5b2701f4600b3604d1e0ab1c02dd1454c47dfd
Abhinav Sharma <abhi18av@gmail.com>
2019-08-02 18:17:01 +0000

    updated the kvarq commands

---
+++ minilab/run_kvarq_on_doubtful_genomes.sh
@@ -0,0 +1,125 @@
+#rclone copy onedrive-em:africanum_analysis/unzipped_fastq_files/SAMEA3359915_1.fastq . -vv
+vagrant ssh -c "cd /vagrant/africanum_analysis/minilab && kvarq scan -l MTBC -p SAMEA3359915_1.fastq  SAMEA3359915_1.json"
+#rclone copy onedrive-em:africanum_analysis/unzipped_fastq_files/SAMEA3359915_2.fastq . -vv
+vagrant ssh -c "cd /vagrant/africanum_analysis/minilab && kvarq scan -l MTBC -p SAMEA3359915_2.fastq  SAMEA3359915_2.json"
+
+#rclone copy onedrive-em:africanum_analysis/unzipped_fastq_files/SAMEA3359872_1.fastq . -vv
+vagrant ssh -c "cd /vagrant/africanum_analysis/minilab && kvarq scan -l MTBC -p SAMEA3359872_1.fastq  SAMEA3359872_1.json"
+#rclone copy onedrive-em:africanum_analysis/unzipped_fastq_files/SAMEA3359872_2.fastq . -vv
+vagrant ssh -c "cd /vagrant/africanum_analysis/minilab && kvarq scan -l MTBC -p SAMEA3359872_2.fastq  SAMEA3359872_2.json"
+
+#rclone copy onedrive-em:africanum_analysis/unzipped_fastq_files/SAMEA3359865_1.fastq . -vv
+vagrant ssh -c "cd /vagrant/africanum_analysis/minilab && kvarq scan -l MTBC -p SAMEA3359865_1.fastq  SAMEA3359865_1.json"
+#rclone copy onedrive-em:africanum_analysis/unzipped_fastq_files/SAMEA3359865_2.fastq . -vv
+vagrant ssh -c "cd /vagrant/africanum_analysis/minilab && kvarq scan -l MTBC -p SAMEA3359865_2.fastq  SAMEA3359865_2.json"
+
+#rclone copy onedrive-em:africanum_analysis/unzipped_fastq_files/SAMEA3359910_1.fastq . -vv
+vagrant ssh -c "cd /vagrant/africanum_analysis/minilab && kvarq scan -l MTBC -p SAMEA3359910_1.fastq  SAMEA3359910_1.json"
+#rclone copy onedrive-em:africanum_analysis/unzipped_fastq_files/SAMEA3359910_2.fastq . -vv
+vagrant ssh -c "cd /vagrant/africanum_analysis/minilab && kvarq scan -l MTBC -p SAMEA3359910_2.fastq  SAMEA3359910_2.json"
+
+#rclone copy onedrive-em:africanum_analysis/unzipped_fastq_files/SAMEA3359887_1.fastq . -vv
+vagrant ssh -c "cd /vagrant/africanum_analysis/minilab && kvarq scan -l MTBC -p SAMEA3359887_1.fastq  SAMEA3359887_1.json"
+#rclone copy onedrive-em:africanum_analysis/unzipped_fastq_files/SAMEA3359887_2.fastq . -vv
+vagrant ssh -c "cd /vagrant/africanum_analysis/minilab && kvarq scan -l MTBC -p SAMEA3359887_2.fastq  SAMEA3359887_2.json"
+
+#rclone copy onedrive-em:africanum_analysis/unzipped_fastq_files/SAMEA3359868_1.fastq . -vv
+vagrant ssh -c "cd /vagrant/africanum_analysis/minilab && kvarq scan -l MTBC -p SAMEA3359868_1.fastq  SAMEA3359868_1.json"
+#rclone copy onedrive-em:africanum_analysis/unzipped_fastq_files/SAMEA3359868_2.fastq . -vv
+vagrant ssh -c "cd /vagrant/africanum_analysis/minilab && kvarq scan -l MTBC -p SAMEA3359868_2.fastq  SAMEA3359868_2.json"
+
+#rclone copy onedrive-em:africanum_analysis/unzipped_fastq_files/SAMEA3359903_1.fastq . -vv
+vagrant ssh -c "cd /vagrant/africanum_analysis/minilab && kvarq scan -l MTBC -p SAMEA3359903_1.fastq  SAMEA3359903_1.json"
+#rclone copy onedrive-em:africanum_analysis/unzipped_fastq_files/SAMEA3359903_2.fastq . -vv
+vagrant ssh -c "cd /vagrant/africanum_analysis/minilab && kvarq scan -l MTBC -p SAMEA3359903_2.fastq  SAMEA3359903_2.json"
+
+#rclone copy onedrive-em:africanum_analysis/unzipped_fastq_files/SAMEA3359869_1.fastq . -vv
+vagrant ssh -c "cd /vagrant/africanum_analysis/minilab && kvarq scan -l MTBC -p SAMEA3359869_1.fastq  SAMEA3359869_1.json"
+#rclone copy onedrive-em:africanum_analysis/unzipped_fastq_files/SAMEA3359869_2.fastq . -vv
+vagrant ssh -c "cd /vagrant/africanum_analysis/minilab && kvarq scan -l MTBC -p SAMEA3359869_2.fastq  SAMEA3359869_2.json"
+
+#rclone copy onedrive-em:africanum_analysis/unzipped_fastq_files/SAMEA3359904_1.fastq . -vv
+vagrant ssh -c "cd /vagrant/africanum_analysis/minilab && kvarq scan -l MTBC -p SAMEA3359904_1.fastq  SAMEA3359904_1.json"
+#rclone copy onedrive-em:africanum_analysis/unzipped_fastq_files/SAMEA3359904_2.fastq . -vv
+vagrant ssh -c "cd /vagrant/africanum_analysis/minilab && kvarq scan -l MTBC -p SAMEA3359904_2.fastq  SAMEA3359904_2.json"
+
+#rclone copy onedrive-em:africanum_analysis/unzipped_fastq_files/SAMEA3359867_1.fastq . -vv
+vagrant ssh -c "cd /vagrant/africanum_analysis/minilab && kvarq scan -l MTBC -p SAMEA3359867_1.fastq  SAMEA3359867_1.json"
+#rclone copy onedrive-em:africanum_analysis/unzipped_fastq_files/SAMEA3359867_2.fastq . -vv
+vagrant ssh -c "cd /vagrant/africanum_analysis/minilab && kvarq scan -l MTBC -p SAMEA3359867_2.fastq  SAMEA3359867_2.json"
+
+#rclone copy onedrive-em:africanum_analysis/unzipped_fastq_files/SAMEA3359871_1.fastq . -vv
+vagrant ssh -c "cd /vagrant/africanum_analysis/minilab && kvarq scan -l MTBC -p SAMEA3359871_1.fastq  SAMEA3359871_1.json"
+#rclone copy onedrive-em:africanum_analysis/unzipped_fastq_files/SAMEA3359871_2.fastq . -vv
+vagrant ssh -c "cd /vagrant/africanum_analysis/minilab && kvarq scan -l MTBC -p SAMEA3359871_2.fastq  SAMEA3359871_2.json"
+
+#rclone copy onedrive-em:africanum_analysis/unzipped_fastq_files/SAMEA3359874_1.fastq . -vv
+vagrant ssh -c "cd /vagrant/africanum_analysis/minilab && kvarq scan -l MTBC -p SAMEA3359874_1.fastq  SAMEA3359874_1.json"
+#rclone copy onedrive-em:africanum_analysis/unzipped_fastq_files/SAMEA3359874_2.fastq . -vv
+vagrant ssh -c "cd /vagrant/africanum_analysis/minilab && kvarq scan -l MTBC -p SAMEA3359874_2.fastq  SAMEA3359874_2.json"
+
+#rclone copy onedrive-em:africanum_analysis/unzipped_fastq_files/SAMEA3359914_1.fastq . -vv
+vagrant ssh -c "cd /vagrant/africanum_analysis/minilab && kvarq scan -l MTBC -p SAMEA3359914_1.fastq  SAMEA3359914_1.json"
+#rclone copy onedrive-em:africanum_analysis/unzipped_fastq_files/SAMEA3359914_2.fastq . -vv
+vagrant ssh -c "cd /vagrant/africanum_analysis/minilab && kvarq scan -l MTBC -p SAMEA3359914_2.fastq  SAMEA3359914_2.json"
+
+#rclone copy onedrive-em:africanum_analysis/unzipped_fastq_files/SAMEA3359875_1.fastq . -vv
+vagrant ssh -c "cd /vagrant/africanum_analysis/minilab && kvarq scan -l MTBC -p SAMEA3359875_1.fastq  SAMEA3359875_1.json"
+#rclone copy onedrive-em:africanum_analysis/unzipped_fastq_files/SAMEA3359875_2.fastq . -vv
+vagrant ssh -c "cd /vagrant/africanum_analysis/minilab && kvarq scan -l MTBC -p SAMEA3359875_2.fastq  SAMEA3359875_2.json"
+
+#rclone copy onedrive-em:africanum_analysis/unzipped_fastq_files/SAMEA3359888_1.fastq . -vv
+vagrant ssh -c "cd /vagrant/africanum_analysis/minilab && kvarq scan -l MTBC -p SAMEA3359888_1.fastq  SAMEA3359888_1.json"
+#rclone copy onedrive-em:africanum_analysis/unzipped_fastq_files/SAMEA3359888_2.fastq . -vv
+vagrant ssh -c "cd /vagrant/africanum_analysis/minilab && kvarq scan -l MTBC -p SAMEA3359888_2.fastq  SAMEA3359888_2.json"
+
+#rclone copy onedrive-em:africanum_analysis/unzipped_fastq_files/SAMEA3359901_1.fastq . -vv
+vagrant ssh -c "cd /vagrant/africanum_analysis/minilab && kvarq scan -l MTBC -p SAMEA3359901_1.fastq  SAMEA3359901_1.json"
+#rclone copy onedrive-em:africanum_analysis/unzipped_fastq_files/SAMEA3359901_2.fastq . -vv
+vagrant ssh -c "cd /vagrant/africanum_analysis/minilab && kvarq scan -l MTBC -p SAMEA3359901_2.fastq  SAMEA3359901_2.json"
+
+#rclone copy onedrive-em:africanum_analysis/unzipped_fastq_files/SAMEA3359898_1.fastq . -vv
+vagrant ssh -c "cd /vagrant/africanum_analysis/minilab && kvarq scan -l MTBC -p SAMEA3359898_1.fastq  SAMEA3359898_1.json"
+#rclone copy onedrive-em:africanum_analysis/unzipped_fastq_files/SAMEA3359898_2.fastq . -vv
+vagrant ssh -c "cd /vagrant/africanum_analysis/minilab && kvarq scan -l MTBC -p SAMEA3359898_2.fastq  SAMEA3359898_2.json"
+
+#rclone copy onedrive-em:africanum_analysis/unzipped_fastq_files/SAMEA3359902_1.fastq . -vv
+vagrant ssh -c "cd /vagrant/africanum_analysis/minilab && kvarq scan -l MTBC -p SAMEA3359902_1.fastq  SAMEA3359902_1.json"
+#rclone copy onedrive-em:africanum_analysis/unzipped_fastq_files/SAMEA3359902_2.fastq . -vv
+vagrant ssh -c "cd /vagrant/africanum_analysis/minilab && kvarq scan -l MTBC -p SAMEA3359902_2.fastq  SAMEA3359902_2.json"
+
+#rclone copy onedrive-em:africanum_analysis/unzipped_fastq_files/SAMEA3359885_1.fastq . -vv
+vagrant ssh -c "cd /vagrant/africanum_analysis/minilab && kvarq scan -l MTBC -p SAMEA3359885_1.fastq  SAMEA3359885_1.json"
+#rclone copy onedrive-em:africanum_analysis/unzipped_fastq_files/SAMEA3359885_2.fastq . -vv
+vagrant ssh -c "cd /vagrant/africanum_analysis/minilab && kvarq scan -l MTBC -p SAMEA3359885_2.fastq  SAMEA3359885_2.json"
+
+#rclone copy onedrive-em:africanum_analysis/unzipped_fastq_files/SAMEA3359897_1.fastq . -vv
+vagrant ssh -c "cd /vagrant/africanum_analysis/minilab && kvarq scan -l MTBC -p SAMEA3359897_1.fastq  SAMEA3359897_1.json"
+#rclone copy onedrive-em:africanum_analysis/unzipped_fastq_files/SAMEA3359897_2.fastq . -vv
+vagrant ssh -c "cd /vagrant/africanum_analysis/minilab && kvarq scan -l MTBC -p SAMEA3359897_2.fastq  SAMEA3359897_2.json"
+
+#rclone copy onedrive-em:africanum_analysis/unzipped_fastq_files/SAMEA3359877_1.fastq . -vv
+vagrant ssh -c "cd /vagrant/africanum_analysis/minilab && kvarq scan -l MTBC -p SAMEA3359877_1.fastq  SAMEA3359877_1.json"
+#rclone copy onedrive-em:africanum_analysis/unzipped_fastq_files/SAMEA3359877_2.fastq . -vv
+vagrant ssh -c "cd /vagrant/africanum_analysis/minilab && kvarq scan -l MTBC -p SAMEA3359877_2.fastq  SAMEA3359877_2.json"
+
+#rclone copy onedrive-em:africanum_analysis/unzipped_fastq_files/SAMEA3504606_1.fastq . -vv
+vagrant ssh -c "cd /vagrant/africanum_analysis/minilab && kvarq scan -l MTBC -p SAMEA3504606_1.fastq  SAMEA3504606_1.json"
+#rclone copy onedrive-em:africanum_analysis/unzipped_fastq_files/SAMEA3504606_2.fastq . -vv
+vagrant ssh -c "cd /vagrant/africanum_analysis/minilab && kvarq scan -l MTBC -p SAMEA3504606_2.fastq  SAMEA3504606_2.json"
+
+#rclone copy onedrive-em:africanum_analysis/unzipped_fastq_files/SAMEA3359876_1.fastq . -vv
+vagrant ssh -c "cd /vagrant/africanum_analysis/minilab && kvarq scan -l MTBC -p SAMEA3359876_1.fastq  SAMEA3359876_1.json"
+#rclone copy onedrive-em:africanum_analysis/unzipped_fastq_files/SAMEA3359876_2.fastq . -vv
+vagrant ssh -c "cd /vagrant/africanum_analysis/minilab && kvarq scan -l MTBC -p SAMEA3359876_2.fastq  SAMEA3359876_2.json"
+
+#rclone copy onedrive-em:africanum_analysis/unzipped_fastq_files/SAMEA3359906_1.fastq . -vv
+vagrant ssh -c "cd /vagrant/africanum_analysis/minilab && kvarq scan -l MTBC -p SAMEA3359906_1.fastq  SAMEA3359906_1.json"
+#rclone copy onedrive-em:africanum_analysis/unzipped_fastq_files/SAMEA3359906_2.fastq . -vv
+vagrant ssh -c "cd /vagrant/africanum_analysis/minilab && kvarq scan -l MTBC -p SAMEA3359906_2.fastq  SAMEA3359906_2.json"
+
+#rclone copy onedrive-em:africanum_analysis/unzipped_fastq_files/SAMEA3359886_1.fastq . -vv
+vagrant ssh -c "cd /vagrant/africanum_analysis/minilab && kvarq scan -l MTBC -p SAMEA3359886_1.fastq  SAMEA3359886_1.json"
+#rclone copy onedrive-em:africanum_analysis/unzipped_fastq_files/SAMEA3359886_2.fastq . -vv
+vagrant ssh -c "cd /vagrant/africanum_analysis/minilab && kvarq scan -l MTBC -p SAMEA3359886_2.fastq  SAMEA3359886_2.json"
+
